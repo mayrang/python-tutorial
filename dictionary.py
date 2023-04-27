7abb81c818f2f0f8467b72106e248c6cba2df217
mayrang <pkss0626@naver.com>
2023-04-27 17:16:33 +0900

    update dictionary example

--- dictionary.py
+++ dictionary.py
@@ -5,8 +5,14 @@
 # 딕셔너리의 내용은 자유롭게 하되 3개 이상의 단어로 구성하시오
 
 # TODO 딕셔너리 생성
-dictionary = 
+dictionary = {"apple": "사과", "banana": "바나나", "cherry": "체리"}
 
 word = input("영어 단어를 입력하세요: ")
+
+if word in dictionary:
+  meaning = dictionary[word]
+  print(f'{word}의 의미는 {meaning}입니다.')
+else:
+  print(f"사전에 {word}가 없습니다.")
 
 # TODO 사전에 단어가 있는지 검사하고, 있는 경우 한글  뜻을 출력하는 문장 완성
